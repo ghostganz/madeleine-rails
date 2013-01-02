d300853af878181db88e02c691578d0928eec614
Anders Bengtsson <ndrsbngtssn@yahoo.se>
2013-01-02 20:48:20 +0100

    Initial import of sample app from madeleine-rack

---
+++ sampleapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sampleapp::Application.config.secret_token = '68c1d38b7bc01a8c1575b07da70b0e65b586dfbaf14f05a29acbe2e7e63ee2f3aea039a79585812035190fc3e922560ebf0debebc6aa42976a184ac4d187e57f'
